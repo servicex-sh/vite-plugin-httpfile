d2060c119a97cf1e6eb85aa994e83e200599c22a
linux_china <libing.chen@gmail.com>
2022-08-10 20:35:44 -0700

    refactor: rename host to baseURL

--- tests/demo-http.d.ts
+++ tests/demo-http.d.ts
@@ -1,6 +1,6 @@
 declare module "*demo.http" {
     export function myIp(): Promise<Response>;
 
-    export function postTest(params: { nick: string, host: string, uuid: string }): Promise<Response>;
+    export function postTest(params: { nick: string, baseURL: string, uuid: string }): Promise<Response>;
 
 }
