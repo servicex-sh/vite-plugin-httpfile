7375b4e9517f10e0b64d8806cfc04b5a212caba0
linux_china <libing.chen@gmail.com>
2022-08-10 19:37:31 -0700

    test: demo.test.ts to test http requests

---
+++ tests/demo.test.ts
@@ -0,0 +1,12 @@
+import {test} from "vitest";
+import {myIp, postTest} from './demo.http'
+
+test("my-ip", async () => {
+    let response = await myIp();
+    console.log(await response.json());
+});
+
+test("post-test", async () => {
+    let response = await postTest({nick: "test", host: "httpbin.org", "uuid": "c8389930-1071-4b88-9676-30b9ba7f2343"});
+    console.log(await response.json());
+});
